a917ddaeb199c5e134e9a5d38673d850f35ca227
LuiccianDev <181299053+LuiccianDev@users.noreply.github.com>
2025-03-27 15:07:08 -0500

    add backedn carpet

---
+++ backend/app.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from routes.download import download_router
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.include_router(download_router, prefix="/api", tags=["download"])
+
+# 🔹 Configurar CORS para permitir solicitudes desde el frontend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # React en Vite usa este puerto
+    allow_credentials=True,
+    allow_methods=["*"],  # Permitir todos los métodos (GET, POST, etc.)
+    allow_headers=["*"],  # Permitir todos los encabezados
+)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, port=8000 ,reload=True)
---
+++ backend/routes/download.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter, BackgroundTasks
+from fastapi.responses import FileResponse
+from services.download_services import descargar_musica, delete_file
+from schema.download_schema import RequestDownload
+
+download_router = APIRouter()
+
+@download_router.post("/download")
+async def download(request: RequestDownload, background_tasks: BackgroundTasks):
+    url = request.url
+    filename = request.filename  # Obtener nombre del usuario
+
+    # Descargar la música con el nombre personalizado
+    file_path = descargar_musica(url, filename)
+
+    if not file_path:
+        return {"message": "Error al descargar la música"}
+
+    
+
+    # Función para eliminar el archivo después de enviarlo
+
+    background_tasks.add_task(delete_file, file_path)
+    
+    # Enviar el archivo y eliminarlo después
+    response = FileResponse(file_path, media_type="audio/mpeg", filename=f"{filename}.mp3")
+
+    return response
